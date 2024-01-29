ca8299ae5aaf50a58c4618ba64c8f6fd32937d62
iGoByNondumiso <nondumisomhlongo.nm@gmail.com>
2024-01-30 00:46:26 +0200

    added js to change temperature according to API

--- functions.js
+++ functions.js
@@ -8,22 +8,39 @@ let minutes = now.getMinutes();
 let days= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 let day = days[theDay]
 
-
-
-
 function todaysTime(event){
   let time = document.querySelector("#today")
   let timeUpdate = `${day} ${hour}:${minutes}`
   time.innerHTML= timeUpdate
 }
 
-
 let doASearch = document.querySelector("#search-button")
 doASearch.addEventListener("submit",todaysTime())
 
+// use weather api to change my weather info per city
+
+function tempUpdate(response){
+  let latestTemp = document.querySelector(".todaysTempFigure")
+  console.log(response.data.temperature.current)
+  let apiOutput = response.data.temperature.current
+  latestTemp.innerHTML = Math.round(apiOutput)
+}
+
+
+let updateCityTemp = document.querySelector(".searchBar")
+updateCityTemp.addEventListener("submit",tempUpdate)
+
 // change city name after new search
 
+function citySearch (city){
+  let searchedCity = document.querySelector("#search-bar-input")
+  let cityEntry = searchedCity.value
+  let apiKey = "79ta47230c920obd7a229aa4aa63f7b7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityEntry}&key=${apiKey}`
+  console.log(apiUrl)
+  axios.get(apiUrl).then(tempUpdate)
 
+}
 
 function cityUpdate(event){
   event.preventDefault()
@@ -31,8 +48,19 @@ function cityUpdate(event){
   console.log(searchedCity.value)
   let myCity = document.querySelector("#city")
   myCity.innerHTML= searchedCity.value
+  citySearch(searchedCity.value)
 
 }
 
 let searchACity = document.querySelector(".searchBar")
 searchACity.addEventListener("submit",cityUpdate)
+
+
+
+
+
+
+
+
+
+
